c8b75e6a42785f00f631b17660c200b4ba41281d
IvanOrsh <ksironov@mail.ru>
2023-12-03 00:30:37 +0300

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+
+const server = Fastify({
+  logger: true,
+});
+
+server.listen(
+  {
+    port: Number(process.env.PORT),
+    host: process.env.HOST,
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    server.log.info("Let's Go!");
+  }
+);
