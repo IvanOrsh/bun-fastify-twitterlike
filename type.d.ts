d21539d20f130cc87d7cac97320c9c77901c521a
IvanOrsh <ksironov@mail.ru>
2023-12-04 01:18:07 +0300

    chore: extend fastify instance

---
+++ type.d.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance as RealFastifyInstance } from "fastify";
+
+import Repository from "./src/repository/Repository";
+
+declare module "fastify" {
+  export interface FastifyInstance extends RealFastifyInstance {
+    repo: Repository;
+  }
+}
