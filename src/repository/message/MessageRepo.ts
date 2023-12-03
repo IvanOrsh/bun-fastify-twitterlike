eda5727c38041c094170051c7fad2dcb38a65cf5
IvanOrsh <ksironov@mail.ru>
2023-12-03 22:12:52 +0300

    feat: message repo

--- src/repository/message/MessageRepo.ts
+++ src/repository/message/MessageRepo.ts
@@ -1,5 +1,137 @@
 import { PrismaClient } from "@prisma/client";
 
+import { SortOrder } from "../Repository";
+
 export default class MessageRepo {
   constructor(private readonly client: PrismaClient) {}
+
+  async selectMessagesFromFollowed(followerId: bigint) {
+    return (
+      await this.client.follow.findMany({
+        select: {
+          followed: {
+            select: {
+              messages: true,
+            },
+          },
+        },
+
+        where: {
+          followerId,
+        },
+      })
+    )
+      .flatMap((item) => item.followed.messages)
+      .sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime());
+  }
+
+  async selectedMessagesFromAuthorId(authorId: bigint) {
+    return await this.client.message.findMany({
+      where: {
+        authorId,
+      },
+
+      orderBy: {
+        updatedAt: SortOrder.Desc,
+      },
+    });
+  }
+
+  async selectMessageResponses(respondedMsgId: bigint) {
+    return (
+      await this.client.messageResponse.findMany({
+        select: {
+          responderMsg: true,
+          respondedMsg: true,
+        },
+
+        where: {
+          respondedMsgId,
+        },
+      })
+    ).sort(
+      (
+        { responderMsg: responder_a, respondedMsg: responded_a },
+        { responderMsg: responder_b, respondedMsg: responded_b }
+      ) => {
+        return (
+          responder_b.updatedAt.getTime() - responder_a.updatedAt.getTime()
+        );
+      }
+    );
+  }
+
+  async selectMessageBroadcast(broadcastMsgId: bigint) {
+    return (
+      await this.client.messageBroadcast.findMany({
+        select: {
+          broadcasterMsg: true,
+          broadcastMsg: true,
+          additionalMessage: true,
+        },
+
+        where: {
+          broadcastMsgId,
+        },
+      })
+    ).sort(
+      (
+        { broadcasterMsg: broadcaster_a, broadcastMsg: broadcast_a },
+        { broadcasterMsg: broadcaster_b, broadcastMsg: broadcast_b }
+      ) => {
+        return (
+          broadcaster_b.updatedAt.getTime() - broadcaster_a.updatedAt.getTime()
+        );
+      }
+    );
+  }
+
+  async insertMessage(
+    authorId: bigint,
+    body: string,
+    image?: Buffer,
+    respondedMsgId?: bigint,
+    broadcastMsgId?: bigint,
+    additionalMessage?: string
+  ) {
+    // invalid scenarios:
+    if (respondedMsgId && broadcastMsgId) {
+      return new Error(
+        "respondedMsgId and broadcastMsgId are mutually exclusive"
+      );
+    }
+
+    if (!broadcastMsgId && additionalMessage) {
+      return new Error("additionalMessage cannot exist without broadcastMsgId");
+    }
+
+    return await this.client.$transaction(async (tx) => {
+      const newMessage = await tx.message.create({
+        data: {
+          authorId,
+          body,
+          image,
+        },
+      });
+
+      if (respondedMsgId) {
+        await tx.messageResponse.create({
+          data: {
+            responderMsgId: newMessage.id,
+            respondedMsgId,
+          },
+        });
+      } else if (broadcastMsgId) {
+        await tx.messageBroadcast.create({
+          data: {
+            broadcasterMsgId: newMessage.id,
+            broadcastMsgId, // broadcasted
+            additionalMessage,
+          },
+        });
+      }
+
+      return newMessage;
+    });
+  }
 }
